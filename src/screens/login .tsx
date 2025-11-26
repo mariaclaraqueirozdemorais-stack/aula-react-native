d5de6cc6db9a852c3ba17e549cd31ecab5707224
clara <mariaclaraqueirozdemorais@gmail.com>
2025-11-26 20:44:39 -0300

    refwctor: alteração da estrutura idenficação tallwind

---
+++ src/screens/login .tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function LoginScreen () {
+   return (
+        <View className="flex-1 items-center bg-red-500 justify-center" >
+            <Text className="text-6xl">tela de Login</Text>
+        </View>
+   )
+}
