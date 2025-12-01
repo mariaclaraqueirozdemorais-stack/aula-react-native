17dcc2c161d12a3e6ad39b16641f5e3e27fa4fb8
clara <mariaclaraqueirozdemorais@gmail.com>
2025-12-01 20:39:33 -0300

    feature: add campo  esqueeu senha

--- src/screens/login .tsx
+++ src/screens/login .tsx
@@ -4,11 +4,17 @@ import { Text, View } from 'react-native';
 
 export function LoginScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <InputCustomizado placeholder={'Login'} />
-      <InputCustomizado placeholder="senha" />
-
-      <ButtonCustomizado title="entrar" />
+    <View className="flex-1 items-center justify-center">
+      <View className="w-full gap-5 bg-red-500 p-5">
+        
+          <Text className="text-3xl">nosso app</Text>
+          
+        <InputCustomizado
+        placeholder={"senha"}
+        />
+        <InputCustomizado placeholder={'Login'} />
+        <ButtonCustomizado title="entrar" />
+      </View>
     </View>
-  );
-}
+  ) 
+
