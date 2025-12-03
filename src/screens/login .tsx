0aa099106d7b45e11f92ff4b1d105c5df652f2fb
clara <mariaclaraqueirozdemorais@gmail.com>
2025-12-02 21:06:56 -0300

    feat a senha invisivel

--- src/screens/login .tsx
+++ src/screens/login .tsx
@@ -1,20 +1,31 @@
 import { ButtonCustomizado } from '@/components/botaoCustomizado';
 import { InputCustomizado } from '@/components/inputcustomizado';
+import { useState } from 'react';
 import { Text, View } from 'react-native';
 
 export function LoginScreen() {
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <View className="flex-1 items-center justify-center">
-      <View className="w-full gap-5 bg-red-500 p-5">
-        
-          <Text className="text-3xl">nosso app</Text>
-          
+      <View className="w-full items-center gap-5 p-5">
+        <Text className="text-3xl"> nosso app </Text>
+
+        <InputCustomizado placeholder={'login'} onChangeText={(text) => setLogin(text)} />
         <InputCustomizado
-        placeholder={"senha"}
+          placeholder={'senha'}
+          onChangeText={(text) => setSenha(text)}
+          secureTextEntry={true}
         />
-        <InputCustomizado placeholder={'Login'} />
-        <ButtonCustomizado title="entrar" />
+
+        <ButtonCustomizado
+          title="Entrar"
+          onPress={() => console.log('login', login, 'senha', senha)}
+        />
+
+        <Text className="text-blue-400">esqueceu a Senha?</Text>
       </View>
     </View>
-  ) 
-
+  );
+}
